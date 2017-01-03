5ba9307e3df5cda61f43f0a393cdba63354ed3fc
John Didion <github@didion.net>
2017-01-03 11:42:43 -0500

    update sort.py

--- bin/seq/sort.py
+++ bin/seq/sort.py
@@ -1,22 +1,39 @@
 #!/bin/env python
-# Sort a SAM or GTF file. Sorting is in-memory, so it is fast but inefficient.
+# Sort a SAM/BED/GTF/GFF file. Sorting is in-memory, so it is fast but inefficient.
 
+import argparse
+from collections import defaultdict, OrderedDict
+import csv
+from functools import cmp_to_key
 import os
 import sys
-import csv
 import re
-import argparse
-import fileinput
-from itertools import tee, imap
-from collections import defaultdict, namedtuple
-
-from jpd.util.io import open_mgr
+from tqdm import tqdm
+from xphyle import open_
+from xphyle.utils import read_delimited, fileinput
+from xphyle.paths import split_path
 
 chr_map = dict(X=23, Y=24, M=25, Un=26)
+
+def map_chrm(row, idx, chrm_map=None):
+    if chrm_map is None:
+        return row
+    if row[idx] in chrm_map:
+        row[idx] = chrm_map[row[idx]]
+    else:
+        return None
 
 # Natural sorting of chromosomes, contigs, and other sequences
 # that may be present in a GTF file
 def compare_seq_names(a, b):
+    def cmp(x, y):
+        if x < y:
+            return -1
+        elif x == y:
+            return 0
+        else:
+            return 1
+    
     if a == b:
         return 0
     
@@ -71,81 +88,144 @@ def compare_seq_names(a, b):
     # default to string sorting
     return cmp(a, b)
 
-# compare two rows on the same chromosome
-def compare_rows(a, b):
-    c = cmp(a.start, b.start)
-    if c == 0:
-        c = cmp(a.end, b.end)
-    if c == 0:
-        c = cmp(a.strand, b.strand)
-    return c
+class Seqname(object):
+    def __init__(self, seqname):
+        self.seqname = seqname
+    
+    def __lt__(self, other):
+        return compare_seq_names(self.seqname, other.seqname) < 0
+    
+    def __repr__(self):
+        return self.seqname
 
-def sort(infiles, output, chrm_map, make_fn, cmp_fn):
+seqname_cache = {}
+def seqname(name):
+    if name not in seqname_cache:
+        seqname_cache[name] = Seqname(name)
+    return seqname_cache[name]
+
+def lt(a, b, fields):
+    for f in fields:
+        a_val = getattr(a, f)
+        b_val = getattr(b, f)
+        if a_val < b_val:
+            return True
+        elif a_val > b_val:
+            return False
+    else:
+        return False
+
+class Record(object):
+    def __init__(self, row, fields, cmp_fields):
+        self.row = row
+        self.fields = fields
+        self.cmp_fields = cmp_fields
+    
+    def __lt__(self, other):
+        return lt(self, other, self.cmp_fields)
+
+    def __getattr__(self, name):
+        field = self.fields[name]
+        value = self.row[field[0]]
+        type = field[1]
+        if value == '.' and type != str:
+            return None
+        return type(value)
+
+GFF_FIELDS = dict(
+    seqname=(0, seqname),
+    source=(1, str),
+    feature=(2, str),
+    start=(3, int),
+    end=(4, int),
+    score=(5, float),
+    strand=(6, str),
+    frame=(7, int),
+    attribute=(8, str)
+)
+GFF_FEATURES = dict(
+    gene=0,
+    transcript=1,
+    exon=2
+)
+
+class Gff(Record):
+    def __init__(self, row):
+        super().__init__(
+            row, GFF_FIELDS, ('start', 'end', 'strand', 'feature_value'))
+    
+    def __getattr__(self, name):
+        if name == 'feature_value':
+            return GFF_FEATURES.get(self.feature, sys.maxsize)
+        else:
+            return super().__getattr__(name)
+
+sequence_region_re = re.compile('##sequence-region ([^ ]+)')
+
+def sort_gff(reader, output, chrm_map):
     seqs = defaultdict(lambda: [])
-    for line in csv.reader(fileinput.input(infiles, mode="rU"), delimiter='\t'):
-        if line[0].startswith("#"):
-            continue
-        obj = make_fn(line, chrm_map)
-        if obj is not None:
+    file_headers = OrderedDict()
+    chrm_headers = {}
+    for line in reader:
+        if line[0].startswith('##'):
+            chrm = sequence_region_re.match(line[0]).group(1)
+            if chrm in chrm_map:
+                chrm = chrm_map[chrm]
+            chrm_headers[chrm] = line[0]
+        elif line[0].startswith('#'):
+            file_headers[line[0]] = None
+        else:
+            assert len(line) >= 3, "BED file must have at least 3 columns"
+            line = map_chrm(line, 0, chrm_map)
+            if not line:
+                continue
+            obj = Gff(line)
             seqs[obj.seqname].append(obj)
     
     # write the contents back to a file
-    with open_mgr(output, 'wb') as o:
-        w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE, doublequote=False)
-        for s in sorted(seqs.keys(), compare_seq_names): 
+    with open_(output, 'w') as o:
+        for line in file_headers:
+            o.write(line)
+        w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE,
+                       doublequote=False)
+        for s in sorted(seqs.keys()):
+            if s in chrm_headers:
+                o.write(chrm_headers[s])
             rows = seqs[s]
-            rows.sort(cmp_fn)
-            w.writerows(rows)
+            rows.sort()
+            w.writerows(r.row for r in rows)
 
-Gtf = namedtuple("Gtf", 
-    ("seqname","source","feature","start","end","score","strand","frame","attribute"))
-def make_gtf(row, chrm_map=None):
-    if chrm_map is not None:
-        if row[0] in chrm_map:
-            row[0] = chrm_map[row[0]]
-        else:
-            return None
-    row[3] = int(row[3])
-    row[4] = int(row[4])
-    return(Gtf(*row))
+BED_FIELDS = dict(
+    seqname=(0, seqname),
+    start=(1, int),
+    end=(2, int),
+    name=(3, str),
+    score=(4, float),
+    strand=(5, str)
+)
 
-GTF_FEATURES = dict(gene=0, transcript=1, exon=2)
-def compare_gtf_rows(a, b):
-    c = compare_rows(a, b)
-    if c == 0:
-        af = GTF_FEATURES.get(a.feature, 3)
-        bf = GTF_FEATURES.get(b.feature, 3)
-        c = cmp(af, bf)
-    return c
-
-def sort_gtf(infiles, output, chrm_map=None):
-    sort(infiles, output, chrm_map, make_fn=make_gtf, cmp_fn=compare_gtf_rows)
-
-Bed = namedtuple("Bed", 
-    ("seqname","start","end","name","score","strand"))
-def make_bed(row, chrm_map=None):
-    assert len(row) >= 3, "BED file must have at least 3 columns"
-    if chrm_map is not None:
-        if row[0] in chrm_map:
-            row[0] = chrm_map[row[0]]
-        else:
-            return None
-    row[1] = int(row[1])
-    row[2] = int(row[2])
-    if len(row) < 6:
-        row = row[0:3] + ['.','1000','.']
-    elif len(row) > 6:
-        row = row[0:6]
-    return(Bed(*row))
-
-def compare_bed_rows(a, b):
-    c = compare_rows(a, b)
-    if c == 0:
-        c = cmp(a.name, b.name)
-    return c
-
-def sort_bed(infiles, output, chrm_map=None):
-    sort(infiles, output, chrm_map, make_fn=make_bed, cmp_fn=compare_bed_rows)
+def sort_bed(reader, output, chrm_map):
+    seqs = defaultdict(lambda: [])
+    for line in reader:
+        if line[0].startswith("#"):
+            continue
+        assert len(line) >= 3, "BED file must have at least 3 columns"
+        line = map_chrm(line, 0, chrm_map)
+        if not line:
+            continue
+        if len(line) < 6:
+            line = line[0:3] + ['.','1000','.']
+        obj = Record(line, BED_FIELDS, ('start', 'end', 'strand', 'name'))
+        seqs[obj.seqname].append(obj)
+    
+    # write the contents back to a file
+    with open_(output, 'w') as o:
+        w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE,
+                       doublequote=False)
+        for s in sorted(seqs.keys()):
+            rows = seqs[s]
+            rows.sort()
+            w.writerows(r.row for r in rows)
 
 class Attr(object):
     def __init__(self, a):
@@ -154,33 +234,35 @@ class Attr(object):
         self.type = a[1]
         self.value = a[2]
 
-def make_sam(row, chrm_map=None):
-    if chrm_map is not None:
-        if row[2] in chrm_map:
-            row[2] = chrm_map[row[2]]
-        else:
-            return None
-    return Sam(row)
+SAM_FIELDS = dict(
+    name=(0, str),
+    flags=(1, int),
+    seqname=(2, seqname),
+    pos=(3, int),
+    mapq=(4, int),
+    cigar=(5, str),
+    rnext=(6, str),
+    pnext=(7, int),
+    tlen=(8, int),
+    seq=(9, str),
+    qual=(10, str)
+)
 
-class Sam(object):
+class Sam(Record):
     def __init__(self, row):
-        self.row = row
+        super(Sam, self).__init__(row, SAM_FIELDS, ('seqname', 'pos', 'strand'))
         self._attributes = None
-        
-    @property
-    def seqname(self): return self.row[2]
-        
-    @property
-    def pos(self): return int(self.row[3])
     
     @property
     def attributes(self):
-        N = len(self.row)
-        if self._attributes is None and N > 11:
+        num_fields = len(self.row)
+        if self._attributes is None and num_fields > 11:
             def parse_attr(a):
                 attr = Attr(a)
                 return (attr.key, attr)
-            self._attributes = dict(parse_attr(self.row[i]) for i in xrange(11, N))
+            self._attributes = dict(
+                parse_attr(self.row[i])
+                for i in xrange(11, num_fields))
         return self._attributes
     
     @property
@@ -189,6 +271,28 @@ class Sam(object):
         if a is not None and "XS" in a:
             return a["XS"].value
         return None
+
+def sort_sam(reader, outfile, header=None, chrm_map=None):
+    header_lines = list(read_delimited(header)) if header else None
+    
+    with open_(outfile, "w") as o:
+        w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE,
+                       doublequote=False)
+        if header_lines is not None:
+            w.writerows(sort_sam_header(header_lines, chrm_map))
+        
+        seqs = defaultdict(lambda: [])
+        for sam_line in reader:
+            sam_line = map_chrm(sam_line, 2, chrm_map)
+            if not sam_line:
+                continue
+            sam = Sam(row)
+            seqs[sam.seqname].append(sam)
+        
+        for s in sorted(seqs.keys()):
+            rows = seqs[s]
+            rows.sort(compare_sam_rows)
+            w.writerows(r.row for r in rows)
 
 def sort_sam_header(rows, chrm_map=None):
     h = defaultdict(lambda: [])
@@ -208,7 +312,7 @@ def sort_sam_header(rows, chrm_map=None):
                     sh[1] = "SN:{0}".format(chrm_map[sn])
                     new_seq_headers.append(sh)
             seq_headers = new_seq_headers
-        retval += sorted(seq_headers, compare_seq_names, lambda x: x[1][3:])
+        retval += sorted(seqname(x[1][3:]) for x in seq_headers)
     if "@RG" in h:
         retval += h["@RG"]
     if "@PG" in h:
@@ -217,40 +321,12 @@ def sort_sam_header(rows, chrm_map=None):
         retval += h["@CO"]
     return retval
 
-def compare_sam_rows(a, b):
-    c = cmp(a.pos, b.pos)
-    if c == 0:
-        c = cmp(a.strand, b.strand)
-    return c
-
-def sort_sam(infiles, outfile, header=None, chrm_map=None):
-    header_lines = None
-    if header is not None:
-        with open(header, "rU") as i:
-            header_lines = list(csv.reader(i, delimiter='\t'))
-    
-    with open_mgr(outfile, "w") as o:
-        w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE, doublequote=False)
-        if header_lines is not None:
-            w.writerows(sort_sam_header(header_lines, chrm_map))
-        
-        seqs = defaultdict(lambda: [])
-        for sam_line in csv.reader(fileinput.input(infiles), delimiter='\t'):
-            sam = make_sam(sam_line, chrm_map)
-            if sam is not None:
-                seqs[sam.seqname].append(sam)
-        
-        for s in sorted(seqs.keys(), compare_seq_names): 
-            rows = seqs[s]
-            rows.sort(compare_sam_rows)
-            for r in rows:
-                w.writerow(r.row)
-
-def filter_sam(infiles, outfile, chrm_list):
+def filter_sam(reader, outfile, chrm_list):
     """Only keep reads on chromosomes in chrm_list"""
-    with open_mgr(outfile, "w") as o:
-        for sam_line in csv.reader(fileinput.input(infiles), delimiter='\t'):
-            w = csv.writer(o, delimiter="\t", quotechar="", quoting=csv.QUOTE_NONE, doublequote=False)
+    with open_(outfile, "w") as o:
+        for sam_line in reader:
+            w = csv.writer(o, delimiter="\t", quotechar="",
+                           quoting=csv.QUOTE_NONE, doublequote=False)
             if sam_line[0].startswith("@"):
                 if sam_line[0] == "@SQ" and sam_line[1][3:] not in chrm_list:
                     continue
@@ -261,50 +337,54 @@ def filter_sam(infiles, outfile, chrm_list):
                     continue
                 w.writerow(sam.row)
 
-FILE_TYPES = ("gtf","bed","sam")
+FILE_TYPES = ("gff", "bed", "sam")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="""Performs proper sorting of GTF""")
+    parser = argparse.ArgumentParser(
+        description="Performs proper sorting of BED, GFF, and SAM files")
     parser.add_argument("-T", "--type", choices=FILE_TYPES, default=None)
-    parser.add_argument("-i", "--input", default="-", help='path to gtf file to sort')
-    parser.add_argument("-o", "--output", default="-", help='path name of properly sorted gtf')
-    parser.add_argument("-H", "--sam_header", default=None, help='Header to prepend when sorting SAM files')
-    parser.add_argument("-I", "--in_place", action="store_true", default=False, 
-        help="When a header file is specified, the output will be stored to that file")
+    parser.add_argument("-o", "--output", default="-",
+        help='path name of properly sorted gtf')
+    parser.add_argument("-H", "--sam_header", default=None,
+        help='Header to prepend when sorting SAM files')
+    parser.add_argument("-I", "--in_place", action="store_true", default=False,
+        help="When a header file is specified, the output will be stored to "
+             "that file")
     parser.add_argument("-m", "--chrm_map", default=None,
-        help="Tab-delimited file with mapping of chromosome names in GTF/SAM to destination names")
+        help="Tab-delimited file with mapping of chromosome names in GTF/SAM "
+             "to destination names")
     parser.add_argument("--filter_only", action="store_true", default=False,
         help="Only filter, don't sort.")
-    parser.add_argument("infiles", nargs="*", help='path to gtf file to sort')
+    parser.add_argument("infiles", nargs="*", default=['-'],
+        help='paths of the files to sort')
     
     args = parser.parse_args()
     
     file_type = args.type
     if file_type is None:
-        file_type = os.path.splitext(args.infiles[0])[1][1:]
+        file_type = split_path(args.infiles[0], False)[-1]
     
     if file_type not in FILE_TYPES:
-        raise Exception("Invalid file type: {0}".format(file_type))    
+        raise Exception("Invalid file type: {0}".format(file_type))
     
     chrm_map = None
     if args.chrm_map is not None:
-        with open(args.chrm_map, "rU") as m:
-            l = list(csv.reader(m, delimiter="\t"))
-            if len(l[0]) == 1:
-                chrm_map = dict((c[0],c[0]) for c in l)
-            else:
-                chrm_map = dict(l)
+        l = list(read_delimited(args.chrm_map))
+        if len(l[0]) == 1:
+            chrm_map = dict((c[0], c[0]) for c in l)
+        else:
+            chrm_map = dict(l)
     
-    infiles = args.infiles if len(args.infiles) > 0 else [args.input]
+    reader = tqdm(read_delimited(fileinput(args.infiles)))
     
     if args.filter_only:
-        filter_sam(infiles, args.output, chrm_map)
-    elif file_type == "gtf":
-        sort_gtf(infiles, args.output, chrm_map)
+        filter_sam(reader, args.output, chrm_map)
+    elif file_type == "gff":
+        sort_gff(reader, args.output, chrm_map)
     elif file_type == "bed":
-        sort_bed(infiles, args.output, chrm_map)
+        sort_bed(reader, args.output, chrm_map)
     elif file_type == "sam":
         output = args.sam_header if args.sam_header and args.in_place else args.output
-        sort_sam(infiles, output, args.sam_header, chrm_map)
+        sort_sam(reader, output, args.sam_header, chrm_map)
     else:
         raise Exception("Invalid file type {0}".format(args.type))
